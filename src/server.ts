e8c04c2c319d64a80f0f7ad35cc5654eb2bd1f7e
Rodolfo <j10rodolfo@gmail.com>
2025-09-10 22:12:25 -0300

    feat: criando camada de controle

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 import express,{Request, Response}from 'express';
+import createApp from './app';
 
-const app = express();
+const app = createApp();
 const PORT = process.env.PORT
-
-app.get('/', (req: Request, res: Response) => {
-  return res.json({message: 'Hello World!'});
-});
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
