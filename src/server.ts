f48fa655719af9f7498ae815460bcead4bfb7a82
Rodolfo <j10rodolfo@gmail.com>
2025-09-10 21:21:29 -0300

    feat: set up Express server with environment configuration
    - Updated package.json to include Express as a dependency and added environment variable support for the server.
    - Modified server.ts to implement a basic Express server that responds with a JSON message.
    - Created a .env file to define the PORT for the server.

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello, Champions!");
+import express,{Request, Response}from 'express';
+
+const app = express();
+const PORT = process.env.PORT
+
+app.get('/', (req: Request, res: Response) => {
+  return res.json({message: 'Hello World!'});
+});
+
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
